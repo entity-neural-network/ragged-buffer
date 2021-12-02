ff6efcc70651a72371c09f837c9b7e97e9867408
Clemens Winter <clemenswinter1@gmail.com>
2021-12-01 18:51:27 -0800

    Implement constructor methods on `RaggedBuffer` class

--- ragged_buffer/__init__.py
+++ ragged_buffer/__init__.py
@@ -8,4 +8,31 @@ ScalarType = TypeVar("ScalarType", bound=np.generic)
 
 
 class RaggedBuffer(Generic[ScalarType]):
-    pass
+    def __init__(self, features: int) -> None:
+        raise ValueError(
+            "RaggedBuffer is an abstract class, use RaggedBufferF32 or RaggedBufferI64"
+        )
+
+    @classmethod
+    def from_array(cls, x: NDArray[ScalarType]) -> "RaggedBuffer[ScalarType]":
+        if x.dtype == np.float32:
+            return RaggedBufferF32.from_array(x)
+        elif x.dtype == np.int64:
+            return RaggedBufferI64.from_array(x)
+        else:
+            raise ValueError(
+                f"Unsupported dtype {x.dtype}. Only float32 and int64 are currently supported."
+            )
+
+    @classmethod
+    def from_flattened(
+        cls, flattened: NDArray[ScalarType], lengths: NDArray[np.int64]
+    ) -> "RaggedBuffer[ScalarType]":
+        if flattened.dtype == np.float32:
+            return RaggedBufferF32.from_flattened(flattened, lengths)
+        elif flattened.dtype == np.int64:
+            return RaggedBufferI64.from_flattened(flattened, lengths)
+        else:
+            raise ValueError(
+                f"Unsupported dtype {flattened.dtype}. Only float32 and int64 are currently supported."
+            )
